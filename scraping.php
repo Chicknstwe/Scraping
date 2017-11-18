6d2441cf324ab468073863581d612765e917b740
Chicknstwe <memerto@gmail.com>
2017-11-18 03:40:11 +0100

    Update scraping.php

--- scraping.php
+++ scraping.php
@@ -1,10 +1,19 @@
-<?php require_once( dirname(__FILE__) . '/app/bootstrap.php' ); ?>
+<?php require_once( dirname(__FILE__) . '/app/bootstrap.php' );
+
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Scraping tool</title> 
 <link rel="stylesheet" type="text/css" href="/css/scraping.css" media="screen" />
 <script src="/js/tools.js"></script>
+<?php
+	require_once('class/class.install.php');
+
+	$web_progress = new Installer();
+?>
 </head>
 <body>
 	<nav class="parent-menu">
@@ -39,15 +48,15 @@
 <br>
 <br>
 <table class="table-fill">
-<form action="<?php echo $_SERVER['REQUEST_URI'] ?>" name="scraping" method="post" enctype="multipart/form-data">
+<form action="<?php echo $_SERVER['REQUEST_URI'] ?>" id="apiform" target="progressFrame" name="scraping" method="post" enctype="multipart/form-data">
 	<tr><th>Scraping tool</th></tr>
-	<tr><td>Number of runs of the script <input type="range" name="exec_n_range" min="1" max="5000" value="50" step="1" id="n_exec" onchange="range_exec.value=value"><output align="center" id="range_exec">50</output></td></tr>
+	<tr><td>Number of runs of the script <input type="range" name="exec_n_range" min="1" max="1000" value="50" step="1" id="n_exec" onchange="range_exec.value=value"><output align="center" id="range_exec">50</output></td></tr>
 	<tr><td><input type="checkbox" id="checkall_selected_webs" onchange="checkAllElements('selected_webs')"> <strong>Select webs to perform scraping</strong><br />
 <?php
 	$s = 0;
 	while($s < sizeof($websites)) {
 ?>
-		<input type="checkbox" name="selected_webs[]" value="<?php echo $websites[$s];?>" /> <?php echo $names[$s];?><br />	
+		<input type="checkbox" name="selected_webs[]" value="<?php echo $websites[$s];?>" /> <?php echo $names[$websites[$s]];?><br />	
 <?php
 		$s++;
 	}
@@ -73,7 +82,7 @@
 	<tr><td><input type="checkbox" name="save_imgs" value="Yes" > Save images<br>
     <input type="checkbox" name="save_docs" value="Yes"> Save documents<br>
 	<input type="checkbox" name="show_info" value="Yes"> Show performance report</td></tr>
-    <tr><td><input type="submit" name="submit" class="amp" value="Run"/></td></tr>
+    <tr><td><input type="submit" name="apisubmit" id="apisubmit" class="amp" value="Run"/></td></tr>
 </form>
 </table>
 <br><br>
@@ -95,6 +104,8 @@ if (isConnected() && sizeof($selected_keywords) > 0) {
 <script language="javascript">alert("The script has been executed.");</script>
 <?php
 
+	$web_progress->placeholder();
+	$web_progress->setLogPath(''); 
 
 	$array_output = array();
 	$img_num=0;
@@ -111,25 +122,37 @@ if (isConnected() && sizeof($selected_keywords) > 0) {
 	$save_doc_test=0;
 	$ejecuciones=0;
 	$sel_websites = $_REQUEST['selected_webs'];
+	
+	$web_progress->setSteps(sizeof($sel_websites) * $n_exec);
+	$web_progress->defineBar('#000000', '#000000');
 		
 while ($q < sizeof($sel_websites)) {
 	 $on_websites = array();
+	 $web_name = $names[$sel_websites[$q]];
 	 array_push($on_websites, $sel_websites[$q]);
 	 $k=0;
 	 while ($k < sizeof($on_websites) && $k < $n_exec) {
 			
 		$base = $sel_websites[$q];
 		$web = $on_websites[$k];
+		if(substr($web, 0, 2) == '//') {
+			$web = 'http:' . $web;
+			$comp_count++;
+		}
 		$output = file_get_contents_curl($web);
 		$var = pc_link_extractor($output);
 		$media_output = array();
 		$docs_output = array();
 		$url_count++;
 		
+
+		
 		$i=0;
 		while ($i < sizeof($var)) {
-
-			if (in_array(substr($var[$i][0], 0, 1), $compo_url)) {
+			if(substr($var[$i][0], 0, 2) == '//') {
+				$var[$i][0] = 'http:' . $var[$i][0];
+				$comp_count++;
+			} elseif (in_array(substr($var[$i][0], 0, 1), $compo_url)) {
 				$var[$i][0] = $base  . "" . $var[$i][0];
 				$comp_count++;
 			}
@@ -138,7 +161,11 @@ while ($q < sizeof($sel_websites)) {
 				$var[$i][1] = trim($var[$i][1]);
 				$var[$i][1] = tag_img_extractor($var[$i][1]);
 				
-				if (in_array(substr($var[$i][1], -4, 4), $media_ext)) {	
+				if (in_array(substr($var[$i][1], -4, 4), $media_ext)) {
+					if(substr($var[$i][1], 0, 2) == '//') {
+						$var[$i][1] = 'http:' . $var[$i][1];
+						$comp_count++;
+					}
 					if (!in_array($var[$i][1], $img_added)) {
 						array_push($media_output, $var[$i][1]);
 						array_push($img_added, $var[$i][1]);
@@ -147,12 +174,20 @@ while ($q < sizeof($sel_websites)) {
 				} 
 				
 				if (in_array(substr($var[$i][0], -4, 4), $media_ext)) {	
+					if(substr($var[$i][0], 0, 2) == '//') {
+						$var[$i][0] = 'http:' . $var[$i][0];
+						$comp_count++;
+					}
 					if (!in_array($var[$i][0], $img_added)) {
 						array_push($media_output, $var[$i][0]);
 						array_push($img_added, $var[$i][0]);
 					}
 					$img_num++;
 				} elseif (in_array(substr($var[$i][0], -4, 4), $docs_ext)) {
+					if(substr($var[$i][0], 0, 2) == '//') {
+						$var[$i][0] = 'http:' . $var[$i][0];
+						$comp_count++;
+					}
 					if (!in_array($var[$i][0], $docs_added)) {
 						array_push($docs_output, $var[$i][0]);
 						array_push($docs_added, $var[$i][0]);
@@ -170,12 +205,20 @@ while ($q < sizeof($sel_websites)) {
 					}
 				}
 			} elseif (in_array(substr($var[$i][0], -4, 4), $docs_ext)) {
+				if(substr($var[$i][0], 0, 2) == '//') {
+					$var[$i][0] = 'http:' . $var[$i][0];
+					$comp_count++;
+				}
 				if (!in_array($var[$i][0], $docs_added)) {
 					array_push($docs_output, $var[$i][0]);
 					array_push($docs_added, $var[$i][0]);
 				}
 				$docs_count++;
-			} elseif (in_array(substr($var[$i][0], -4, 4), $media_ext)) {	
+			} elseif (in_array(substr($var[$i][0], -4, 4), $media_ext)) {
+				if(substr($var[$i][0], 0, 2) == '//') {
+					$var[$i][0] = 'http:' . $var[$i][0];
+					$comp_count++;
+				}			
 				if (!in_array($var[$i][0], $img_added)) {
 					array_push($media_output, $var[$i][0]);
 					array_push($img_added, $var[$i][0]);
@@ -190,10 +233,10 @@ while ($q < sizeof($sel_websites)) {
 		$i=0;
 		while ($i < sizeof($selected_keywords)) {
 			if (strpos($output, $selected_keywords[$i]) !== false) {
-				$path_base = $path . "/" . $names[$q];
-				if (!file_exists($path)) mkdir(utf8_decode($path), 0777, true);
-				if (!file_exists($path_base)) mkdir(utf8_decode($path_base), 0777, true);
-				$scrapped = fopen($path . '/' . $names[$q] . '/' . valid_chars($on_websites[$k]) . '.html', "w") or die("¡Error opening " . $path_base . "/" . basename($on_websites[$k]) . ".html!");
+				$path_base = $path . "/" . modSpace($web_name);
+				if (!file_exists($path)) mkdir($path, 0777, true);
+				if (!file_exists($path_base)) mkdir($path_base, 0777, true);
+				$scrapped = fopen($path . '/' . modSpace($web_name) . '/' . valid_chars($on_websites[$k]) . '.html', "w") or die("¡Error opening " . $path_base . "/" . basename(modSpace($on_websites[$k])) . ".html!");
 				fwrite($scrapped, $output);
 				fclose($scrapped);
 				$key_matches++;
@@ -210,12 +253,16 @@ while ($q < sizeof($sel_websites)) {
 					}
 					$z=0;
 					while ($z < sizeof($media_output)) {
+						if(substr($media_output[$z], 0, 2) == '//') {
+							$media_output[$z] = 'http:' . $media_output[$z];
+							$comp_count++;
+						}
 						$img_file = file_get_contents_curl($media_output[$z]);
 						$media_path = $path_base . "/imgs";
-						if (!file_exists($path)) mkdir(utf8_decode($path), 0777, true);
-						if (!file_exists($path_base)) mkdir(utf8_decode($path_base), 0777, true);
-						if (!file_exists($media_path)) mkdir(utf8_decode($media_path), 0777, true);
-						$get_img = fopen($media_path . "/" . valid_chars(basename(urldecode($media_output[$z]))), "w") or die("¡Error opening " . $media_path . "/" . basename(urldecode($media_output[$z])));
+						if (!file_exists($path)) mkdir($path, 0777, true);
+						if (!file_exists($path_base)) mkdir($path_base, 0777, true);
+						if (!file_exists($media_path)) mkdir($media_path, 0777, true);
+						$get_img = fopen($media_path . "/" . valid_chars(basename(modSpace($media_output[$z]))), "w") or die("¡Error opening " . $media_path . "/" . basename(modSpace($media_output[$z])));
 						fwrite($get_img, $img_file);
 						fclose($get_img);
 						$save_img_test++;
@@ -225,12 +272,16 @@ while ($q < sizeof($sel_websites)) {
 				if (isset($_POST['save_docs']) && $_POST['save_docs'] == 'Yes') {
 					$z=0;
 					while ($z < sizeof($docs_output)) {
+						if(substr($docs_output[$z], 0, 2) == '//') {
+							$docs_output[$z] = 'http:' . $docs_output[$z];
+							$comp_count++;
+						}
 						$doc_file = file_get_contents_curl($docs_output[$z]);
 						$docs_path = $path_base . "/docs";
-						if (!file_exists($path)) mkdir(utf8_decode($path), 0777, true);
-						if (!file_exists($path_base)) mkdir(utf8_decode($path_base), 0777, true);
-						if (!file_exists($docs_path)) mkdir(utf8_decode($docs_path), 0777, true);
-						$get_doc = fopen($docs_path . "/" . valid_chars(basename(urldecode($docs_output[$z]))), "w") or die("¡Error opening " . $docs_path . "/" . basename($docs_output[$z]));
+						if (!file_exists($path)) mkdir($path, 0777, true);
+						if (!file_exists($path_base)) mkdir($path_base, 0777, true);
+						if (!file_exists($docs_path)) mkdir($docs_path, 0777, true);
+						$get_doc = fopen($docs_path . "/" . valid_chars(basename(modSpace($docs_output[$z]))), "w") or die("¡Error opening " . $docs_path . "/" . basename(modSpace($docs_output[$z])));
 						fwrite($get_doc, $doc_file);
 						fclose($get_doc);
 						$save_doc_test++;
@@ -242,11 +293,15 @@ while ($q < sizeof($sel_websites)) {
 			$i++;
 		}
 		$k++;
+		 $bar_msg = 'Scraping ' . $web_name;
+		 $web_progress->save($bar_msg);
+		$web_progress->delay(0.0);
 		$ejecuciones++;
 	}
 	$k=0;
 	$q++;
 }
+$web_progress->clearTemp(); 
 ?>
 
 <?php
